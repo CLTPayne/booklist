204867a788f9887ad91038c6d1dd2b26b4a7c1a9
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-07-15 19:54:18 +0100

    Fix access to Book database
    * Book class being inside the Booklist Sinatra::Base controller did not
    allow access to create_index or create book objects for the database
    * Book class refactored outside of controller

--- server.rb
+++ server.rb
@@ -3,22 +3,22 @@ require 'mongoid'
 
 Mongoid.load! "mongoid.config"
 
+class Book
+  include Mongoid::Document
+
+  field :title, type: String
+  field :author, type: String
+  field :isbn, type: String
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :isbn, presence: true
+
+  index({ title: 'text' })
+  index({ isbn:1 }, { unique: true, name: "isbn_index" })
+end
+
 class Booklist < Sinatra::Base
-
-  class Book
-    include Mongoid::Document
-
-    field :title, type: String
-    field :author, type: String
-    field :isbn, type: String
-
-    validates :title, presence: true
-    validates :author, presence: true
-    validates :isbn, presence: true
-
-    index({ title: 'text' })
-    index({ isbn:1 }, { unique: true, name: "isbn_index" })
-  end
 
   get '/' do
     "Welcome to Charlotte's Booklist"
